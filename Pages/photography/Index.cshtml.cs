aa09e11b38700702c48d8e3ce7dabb83cee54ab9
Manchahat <105305664+Manchahat@users.noreply.github.com>
2022-06-24 13:47:37 -0400

    added a search string to the index page

--- Pages/photography/Index.cshtml.cs
+++ Pages/photography/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WEB315_Summer22_Assignment.Models;
 
@@ -19,10 +20,33 @@ namespace WEB315_Summer22_Assignment.Pages_photography
         }
 
         public IList<photography> photography { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Types { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string photographyTypes { get; set; }
 
         public async Task OnGetAsync()
         {
-            photography = await _context.photography.ToListAsync();
+        IQueryable<string> TypesQuery = from p in _context.photography
+                                orderby p.Type
+                                select p.Type;
+
+        var photographys = from p in _context.photography
+                select p;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                photographys = photographys.Where(s => s.Name.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(photographyTypes))
+            {
+                photographys = photographys.Where(x => x.Type == photographyTypes);
+            }
+            Types = new SelectList(await TypesQuery.Distinct().ToListAsync());
+            photography = await photographys.ToListAsync();
         }
+        
     }
 }
