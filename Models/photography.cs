0d47c4b8a44929eaba432af73de73847b4148662
Manchahat <105305664+Manchahat@users.noreply.github.com>
2022-06-24 13:38:16 -0400

    updated the pages

--- Models/photography.cs
+++ Models/photography.cs
@@ -7,11 +7,15 @@ namespace WEB315_Summer22_Assignment .Models
     public class photography
     {
         public int ID { get; set; }
+        [Display(Name = "Image Name")]
         public string Name{ get; set; } 
 
         [DataType(DataType.Date)]
         public DateTime Visited  { get; set; }
+
         public string Location{ get; set; }
+
+        [Display(Name = "Image Type")]
         public string Type{ get; set; } 
     }
 }
