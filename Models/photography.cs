58e28e61ef397986f488669b7e6b9aeca952e19e
Manchahat <105305664+Manchahat@users.noreply.github.com>
2022-06-24 13:57:18 -0400

    added validations as required

--- Models/photography.cs
+++ Models/photography.cs
@@ -7,16 +7,25 @@ namespace WEB315_Summer22_Assignment .Models
     public class photography
     {
         public int ID { get; set; }
+
         [Display(Name = "Image Name")]
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$",ErrorMessage="Start with a Capital Letter")]
+         [StringLength(100, MinimumLength = 2)]
         public string Name{ get; set; } 
 
         [DataType(DataType.Date)]
+        [Required]
         public DateTime Visited  { get; set; }
 
+        [Required]
+         [StringLength(100, MinimumLength = 4)]
         public string Location{ get; set; }
 
         [Display(Name = "Image Type")]
-        public string Type{ get; set; } 
+        [Required]
+        public string Type{ get; set; }
+
         public string Rating { get; set; }
     }
 }
