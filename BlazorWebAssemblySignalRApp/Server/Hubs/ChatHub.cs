93e8019c35b663e601c83e2bf8782099a30c83c4
Manchahat <105305664+Manchahat@users.noreply.github.com>
2022-07-23 21:11:20 -0400

    updated the chatpage and chathub with the connectors

--- BlazorWebAssemblySignalRApp/Server/Hubs/ChatHub.cs
+++ BlazorWebAssemblySignalRApp/Server/Hubs/ChatHub.cs
@@ -9,5 +9,13 @@ namespace BlazorWebAssemblySignalRApp.Server.Hubs
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
+        public async Task SendingOthers(string user)
+        {
+            await Clients.Others.SendAsync("UserTyping", user);
+        }
+        public async Task sendingAll(string message)
+        {
+            await Clients.All.SendAsync("ReceiveAnonMessage", message);
+        }
     }
 }
